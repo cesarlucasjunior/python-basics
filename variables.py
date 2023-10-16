beb15e9f2c72f78c58dfed8a2e2bb9a4b06dd31d
César Lucas Júnior <cesarlucasjunior93@gmail.com>
2023-10-16 08:47:03 -0300

    Variable, simple and multiple assignment

---
+++ variables.py
@@ -0,0 +1,13 @@
+# python has a dynamic typing
+n = 0
+print(n)
+
+# this means that we can assign and reassign values to a variable in runtime
+n = "abc"
+print(n) 
+
+# multiple assignments
+
+n,m,z = 0.125, "abc", False
+print(n, '|', m, '|', z)
+
