b2b03f7034715f158fe80d5ae140ccf3459cebfa
César Lucas Júnior <cesarlucasjunior93@gmail.com>
2023-11-06 20:53:57 -0300

    New playground file

---
+++ playing_2.py
@@ -0,0 +1,26 @@
+# Crie uma variável com elementos de 1 a 10
+# Crie uma sublista de lista com os 5 primeiros elementos
+# Para cada elemento do array, jogue para uma variável
+
+lista = [i for i in range(1,11)]
+print(f'Lista criada - {lista}')
+
+sublista = lista[:5] #slicing
+print(f'Sublista - {sublista}')
+
+a, b, c, d, e = sublista #unpacking
+print(f'A - {a}, B - {b}, C - {c}, D - {d}, E - {e}')
+
+
+# Percorra cada elemento da lista
+
+for elemento in lista:
+    print(f'Elemento - {elemento}')
+
+for indice in range(len(lista)):
+    print(f'Índice {indice} - {lista[indice]}')
+
+for indice, elemento in enumerate(lista):
+    print(f'{indice} - {elemento}')
+
+
